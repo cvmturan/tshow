e284a59cc2a08cbde73ceb58dad20d392d7e2508
cvmturan <38521778+cvmturan@users.noreply.github.com>
2026-09-11 20:13:32 +0530

    Repair browser bundle publication

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const SHELL_CACHE = 'tshow-shell-v26';
+const SHELL_CACHE = 'tshow-shell-v27';
 const SHELL_FILES = [
   '/',
   '/index.html',
@@ -9,7 +9,7 @@ const SHELL_FILES = [
   '/css/accounts.css?v=20260911-3',
   '/css/main.css?v=20260911-6',
   '/js/cloudflare-context.js?v=20260911-4',
-  '/js/app.js?v=20260911-8',
+  '/js/app.js?v=20260911-9',
   '/manifest.webmanifest',
   '/assets/tshow-logo.png',
   '/assets/tshow-icon-192.png',
